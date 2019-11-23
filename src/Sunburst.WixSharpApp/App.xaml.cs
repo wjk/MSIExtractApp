1efbdb570568917cb222997484fc6fee73eaeccc
William Kent <wjk@users.noreply.github.com>
2019-11-23 15:36:17 -0500

    Add WPF app project

---
+++ src/Sunburst.WixSharpApp/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) William Kent. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sunburst.WixSharpApp
+{
+    /// <summary>
+    /// The main application subclass.
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ src/Sunburst.WixSharpApp/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) William Kent. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sunburst.WixSharpApp
+{
+    /// <summary>
+    /// The main window for the application.
+    /// </summary>
+    public partial class MainWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
