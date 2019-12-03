48c45c76e8bd74cc020afbaeaa5f31bc3226f4c8
William Kent <wjk@users.noreply.github.com>
2019-12-03 18:44:55 -0500

    Add ExtractFilesView skeleton

---
+++ src/MSIExtract/Views/ExtractFilesView.xaml.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) William Kent. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MSIExtract.Views
+{
+    /// <summary>
+    /// Interaction logic for the "Extract Files" control.
+    /// </summary>
+    public partial class ExtractFilesView : UserControl
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExtractFilesView"/> class.
+        /// </summary>
+        public ExtractFilesView()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectAllCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            FileListView.SelectAll();
+        }
+    }
+}
