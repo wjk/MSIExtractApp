f4e4ca58d0909c959d9360d04a3e9ff1d3e4dbbc
William Kent <wjk@users.noreply.github.com>
2019-12-03 18:36:00 -0500

    Add FilePicker control and its dependencies

---
+++ src/MSIExtract/Controls/FilePicker.cs
@@ -0,0 +1,167 @@
+﻿// Copyright (c) William Kent. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using MSIExtract.Interop;
+
+namespace MSIExtract.Controls
+{
+    /// <summary>
+    /// Provides a control to choose a file from the filesystem.
+    /// </summary>
+    [TemplatePart(Name = "PART_Icon", Type = typeof(Image))]
+    [TemplatePart(Name = "PART_ChooseButton", Type = typeof(Button))]
+    public class FilePicker : Control
+    {
+        /// <summary>
+        /// Provides identity for the <see cref="File"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty FileProperty = DependencyProperty.Register(
+            nameof(File),
+            typeof(FileInfo),
+            typeof(FilePicker),
+            new FrameworkPropertyMetadata(
+                propertyChangedCallback: (obj, args) => ((FilePicker)obj).FilePropertyChanged((FileInfo)args.NewValue),
+                coerceValueCallback: (obj, val) => CoerceFile(val)));
+
+        /// <summary>
+        /// Provides identity for the <see cref="IsReadOnly"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty IsReadOnlyProperty = DependencyProperty.Register(nameof(IsReadOnly), typeof(bool), typeof(FilePicker), new FrameworkPropertyMetadata(false));
+
+        private Image? iconPart;
+        private Button? chooseButtonPart;
+
+        static FilePicker()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FilePicker), new FrameworkPropertyMetadata(typeof(FilePicker)));
+        }
+
+        /// <summary>
+        /// Gets or sets the selected file.
+        /// </summary>
+        public FileInfo? File
+        {
+            get => (FileInfo?)GetValue(FileProperty);
+            set => SetValue(FileProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the user can change the selection.
+        /// </summary>
+        public bool IsReadOnly
+        {
+            get => (bool)GetValue(IsReadOnlyProperty);
+            set => SetValue(IsReadOnlyProperty, value);
+        }
+
+        /// <inheritdoc/>
+        [SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "Message for non-user-facing exception")]
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            if (chooseButtonPart != null)
+            {
+                chooseButtonPart.Click -= ChooseButtonPart_Click;
+            }
+
+            if (iconPart != null)
+            {
+                iconPart.SizeChanged -= IconPart_SizeChanged;
+            }
+
+            iconPart = (Image)GetTemplateChild("PART_Icon");
+            chooseButtonPart = (Button)GetTemplateChild("PART_ChooseButton");
+
+            if (iconPart == null || chooseButtonPart == null)
+            {
+                throw new InvalidOperationException("Invalid control template: required template parts not found");
+            }
+
+            chooseButtonPart.Click += ChooseButtonPart_Click;
+            iconPart.SizeChanged += IconPart_SizeChanged;
+
+            FilePropertyChanged(File);
+        }
+
+        private static FileInfo? CoerceFile(object value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+            else if (value is FileInfo info)
+            {
+                return info;
+            }
+            else if (value is string path)
+            {
+                return new FileInfo(path);
+            }
+            else
+            {
+                throw new InvalidCastException($"Cannot coerce value of type {value.GetType().FullName} to type System.IO.FileInfo");
+            }
+        }
+
+        private void IconPart_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            FilePropertyChanged(File);
+        }
+
+        private void ChooseButtonPart_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void FilePropertyChanged(FileInfo? newValue)
+        {
+            if (iconPart == null)
+            {
+                // If the icon part wasn't retrieved yet, there's nothing to do here.
+                return;
+            }
+
+            if (newValue != null)
+            {
+                NativeMethods.IShellItem2 shellItem = NativeMethods.SHCreateItemFromParsingName(newValue.FullName, IntPtr.Zero, typeof(NativeMethods.IShellItem2).GUID);
+
+                var window = Window.GetWindow(this);
+                uint scale = NativeMethods.GetDpiForWindow(new WindowInteropHelper(window).Handle) / 96;
+                var iconSize = new NativeMethods.SIZE((int)(iconPart.ActualWidth * scale), (int)(iconPart.ActualHeight * scale));
+
+                var imageFactory = (NativeMethods.IShellItemImageFactory)shellItem;
+                imageFactory.GetImage(iconSize, NativeMethods.SIIGBF.RESIZETOFIT | NativeMethods.SIIGBF.ICONONLY, out IntPtr hBitmap);
+
+                try
+                {
+                    iconPart.Source = Imaging.CreateBitmapSourceFromHBitmap(hBitmap, IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+                    iconPart.Visibility = Visibility.Visible;
+                }
+                finally
+                {
+                    NativeMethods.DeleteObject(hBitmap);
+                }
+            }
+            else
+            {
+                iconPart.Source = null;
+            }
+        }
+    }
+}
---
+++ src/MSIExtract/Interop/NativeMethods.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) William Kent. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace MSIExtract.Interop
+{
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1601:Partial elements should be documented", Justification = "not worth it for interop code")]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "not worth it for interop code")]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1602:Enumeration items should be documented", Justification = "not worth it for interop code")]
+    internal static partial class NativeMethods
+    {
+        internal enum SIGDN : uint
+        {
+            NORMALDISPLAY = 0x00000000,
+            PARENTRELATIVEPARSING = 0x80018001,
+            DESKTOPABSOLUTEPARSING = 0x80028000,
+            PARENTRELATIVEEDITING = 0x80031001,
+            DESKTOPABSOLUTEEDITING = 0x8004c000,
+            FILESYSPATH = 0x80058000,
+            URL = 0x80068000,
+            PARENTRELATIVEFORADDRESSBAR = 0x8007c001,
+            PARENTRELATIVE = 0x80080001,
+        }
+
+        internal enum SIIGBF : int
+        {
+            RESIZETOFIT = 0,
+            BIGGERSIZEOK = 0x1,
+            MEMORYONLY = 0x2,
+            ICONONLY = 0x4,
+            THUMBNAILONLY = 0x8,
+            INCACHEONLY = 0x10,
+            CROPTOSQUARE = 0x20,
+            WIDETHUMBNAILS = 0x40,
+            ICONBACKGROUND = 0x80,
+            SCALEUP = 0x100,
+        }
+
+        [ComImport]
+        [Guid("43826D1E-E718-42EE-BC55-A1E261C37BFE")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IShellItem
+        {
+            [return: MarshalAs(UnmanagedType.Interface)]
+            object BindToHandler([In, MarshalAs(UnmanagedType.Interface)] object pbc, [In] ref Guid bhid, [In] ref Guid riid);
+
+            void GetParent([MarshalAs(UnmanagedType.Interface)] out IShellItem ppsi);
+
+            void GetDisplayName([In] SIGDN sigdnName, [MarshalAs(UnmanagedType.LPWStr)] out string ppszName);
+
+            void GetAttributes([In] uint sfgaoMask, out uint psfgaoAttribs);
+
+            void Compare([In, MarshalAs(UnmanagedType.Interface)] IShellItem psi, [In] uint hint, out int piOrder);
+        }
+
+        [ComImport]
+        [Guid("7E9FB0D3-919F-4307-AB2E-9B1860310C93")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IShellItem2 : IShellItem
+        {
+            [return: MarshalAs(UnmanagedType.Interface)]
+            object GetPropertyStore(uint flags, [In] ref Guid riid);
+
+            [return: MarshalAs(UnmanagedType.Interface)]
+            object GetPropertyStoreWithCreateObject(uint flags, [MarshalAs(UnmanagedType.IUnknown)] object punkCreateObject, [In] ref Guid riid);
+
+            [return: MarshalAs(UnmanagedType.Interface)]
+            object GetPropertyStoreForKeys(IntPtr rgKeys, uint cKeys, uint flags, [In] ref Guid riid);
+
+            [return: MarshalAs(UnmanagedType.Interface)]
+            object GetPropertyDescriptionList(IntPtr keyType, [In] ref Guid riid);
+
+            void Update([MarshalAs(UnmanagedType.IUnknown)] object pbc);
+
+            void GetProperty(IntPtr key, out IntPtr pv);
+
+            Guid GetCLSID(IntPtr key);
+
+            System.Runtime.InteropServices.ComTypes.FILETIME GetFileTime(IntPtr key);
+
+            int GetInt32(IntPtr key);
+
+            [return: MarshalAs(UnmanagedType.LPWStr)]
+            string GetString(IntPtr key);
+
+            uint GetUInt32(IntPtr key);
+
+            ulong GetUInt64(IntPtr key);
+
+            [return: MarshalAs(UnmanagedType.Bool)]
+            bool GetBool(IntPtr key);
+        }
+
+        [ComImport]
+        [Guid("bcc18b79-ba16-442f-80c4-8a59c30c463b")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IShellItemImageFactory
+        {
+            void GetImage(SIZE size, SIIGBF flags, out IntPtr hBitmap);
+        }
+
+        [DllImport("user32.dll")]
+        public static extern uint GetDpiForWindow(IntPtr hWnd);
+
+        [DllImport("gdi32.dll")]
+        public static extern void DeleteObject(IntPtr hGdiObject);
+
+        [DllImport("shell32.dll", CharSet = CharSet.Unicode, PreserveSig = false)]
+        public static extern IShellItem2 SHCreateItemFromParsingName(string pszPath, IntPtr pbc, Guid iid);
+
+        internal struct SIZE
+        {
+            public int cx;
+            public int cy;
+
+            public SIZE(int cx, int cy)
+            {
+                this.cx = cx;
+                this.cy = cy;
+            }
+        }
+    }
+}
