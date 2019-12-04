aaf55a2156c7ea73eefac0d8746a73afe2db2642
William Kent <wjk@users.noreply.github.com>
2019-12-03 20:51:51 -0500

    Add handler for File/Exit menu item

--- src/MSIExtract/MainWindow.xaml.cs
+++ src/MSIExtract/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MSIExtract.Controls;
 
 namespace MSIExtract
 {
@@ -30,6 +31,11 @@ namespace MSIExtract
         {
             this.InitializeComponent();
             DataContext = new AppModel();
+        }
+
+        private void CloseWindowCommand_Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
