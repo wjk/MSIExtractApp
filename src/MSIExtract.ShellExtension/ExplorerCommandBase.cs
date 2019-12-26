2f2aa18139732ccc4b83579edfa544d40da4de10
William Kent <wjk@users.noreply.github.com>
2019-12-26 18:41:01 -0500

    Add MSIExtract.ShellExtension project

---
+++ src/MSIExtract.ShellExtension/ExplorerCommandBase.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) William Kent. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MSIExtract.ShellExtension
+{
+    /// <summary>
+    /// This class is the base class of all C# Windows Shell commands.
+    /// </summary>
+    /// <remarks>
+    /// This class abstracts away the underlying COM interfaces,
+    /// and exposes a more C#-like API.
+    /// </remarks>
+    public abstract class ExplorerCommandBase
+    {
+    }
+}
