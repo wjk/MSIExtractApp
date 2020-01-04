60c66961f82c0ddd169d159ccb6e4341f172aba2
William Kent <wjk@users.noreply.github.com>
2020-01-04 15:13:07 -0500

    Add SHellExtension.Loader project

---
+++ src/MSIExtract.ShellExtension.Loader/AssemblyInfo.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"MSI Viewer COM Loader")];
+[assembly:AssemblyCompanyAttribute(L"William Kent")];
+[assembly:AssemblyProductAttribute(L"MSI Viewer")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright © William Kent. All rights reserved.")];
+[assembly:AssemblyVersionAttribute("1.0.0.0")];
+
+[assembly:CLSCompliantAttribute(true)];
+[assembly:ComVisible(false)];
