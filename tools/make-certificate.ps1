42ee3b90dcbb14e3b73467c2c88cbb348ff7caab
William Kent <wjk011@gmail.com>
2020-09-08 07:32:38 -0400

    Fix errors in make-certificate.ps1

--- tools/make-certificate.ps1
+++ tools/make-certificate.ps1
@@ -13,11 +13,12 @@ param(
 #      attribute in src\Sunburst.WixSharpApp.AppxPackage\Package.appxmanifest
 $signer_name = 'CN=382B267D-6047-4C7A-8414-C3EC3B88FF82'
 
-Import-Module PKI
+Import-Module PKI -SkipEditionCheck
 $cert = New-SelfSignedCertificate -Type Custom -Subject $signer_name -KeyUsage DigitalSignature `
 	-FriendlyName "Test Certificate for MSI Viewer app" `
 	-CertStoreLocation "Cert:\CurrentUser\My" `
-	-TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+	-TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}") `
+	-KeyExportPolicy Exportable
 
 $pwd = ConvertTo-SecureString $Password -AsPlainText
 Export-PfxCertificate -Cert $cert -Password $pwd -FilePath $OutputPath
