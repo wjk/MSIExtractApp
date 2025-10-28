48217eae8a965f8a98d29b08e4e36b09e219096a
William Kent <wjk011@gmail.com>
2025-10-28 13:37:06 -0400

    Add XmlnsDefinitionAttribute to AcrylicWindow project

---
+++ src/AcrylicWindow/AssemblyAttributes.cs
@@ -0,0 +1,6 @@
+﻿// Copyright (c) William Kent. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+
+using System.Windows.Markup;
+
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/winfx/2006/xaml/presentation", "AcrylicWindow")]
