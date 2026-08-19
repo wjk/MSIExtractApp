6aecc667452ef6259c183fa12b553fa0b84f7593
William Kent <wjk011@gmail.com>
2026-08-19 16:58:02 -0400

    Remove dead code

--- src/MSIExtract/App.xaml.cs
+++ src/MSIExtract/App.xaml.cs
@@ -11,11 +11,16 @@ using System.Runtime.InteropServices.Marshalling;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+
 using KPreisser.UI;
+
 using MSIExtract.ShellExtension;
 using MSIExtract.Views;
+
 using PresentationTheme.Aero;
+
 using Shmuelie.WinRTServer;
+
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 
@@ -39,54 +44,13 @@ namespace MSIExtract
 
             AeroTheme.SetAsCurrentTheme();
 
-            IActivatedEventArgs? activationArgs = null;
-
-            try
+            if (e != null && e.Args.Length > 0)
             {
-                activationArgs = AppInstance.GetActivatedEventArgs();
-            }
-            catch (System.Runtime.InteropServices.COMException)
-            {
-                activationArgs = null;
-            }
-
-            if (activationArgs != null)
-            {
-                switch (activationArgs.Kind)
-                {
-                    case ActivationKind.Launch:
-                        this.MainWindow = new MainWindow();
-                        break;
-
-                    case ActivationKind.File:
-                        var fileArgs = (FileActivatedEventArgs)activationArgs;
-
-                        if (fileArgs.Files.Count > 0)
-                        {
-                            this.MainWindow = new MainWindow(fileArgs.Files[0].Path);
-                        }
-                        else
-                        {
-                            this.MainWindow = new MainWindow();
-                        }
-
-                        break;
-
-                    default:
-                        this.MainWindow = new MainWindow();
-                        break;
-                }
+                this.MainWindow = new MainWindow(e.Args[0]);
             }
             else
             {
-                if (e != null && e.Args.Length > 0)
-                {
-                    this.MainWindow = new MainWindow(e.Args[0]);
-                }
-                else
-                {
-                    this.MainWindow = new MainWindow();
-                }
+                this.MainWindow = new MainWindow();
             }
 
             this.MainWindow.Show();
